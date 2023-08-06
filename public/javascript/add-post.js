62ae3a25c17252f85aee408c04de1dc7e1b5f726
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-06 11:08:41 -0600

    editAdd-post

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,27 +1,35 @@
 async function newFormHandler(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[name="post-title"]').value;
-    const content = document.querySelector('input[name="content"]').value;    
-  
-    const response = await fetch(`/api/posts`, {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        content
-      }),
-      headers: {
-        'Content-Type': 'application/json'
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const content = document.querySelector('textarea[name="content"]').value;    
+
+  try {
+      const response = await fetch(`/api/posts`, {
+          method: 'POST',
+          body: JSON.stringify({
+              title,
+              content
+          }),
+          headers: {
+              'Content-Type': 'application/json'
+          }
+      });
+
+      if (response.ok) {
+          document.location.replace('/dashboard');
+      } else {
+          const errorResponse = await response.json();
+          alert(errorResponse.message); // Assuming your API sends an error message in the response
       }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
-    }
-  };
+  } catch (error) {
+      console.error('Error:', error);
+      alert('An error occurred while submitting the form.');
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
   console.log('Before addEventListener');
   document.querySelector('#post-form').addEventListener('submit', newFormHandler);
   console.log('After addEventListener');
-  
+});
