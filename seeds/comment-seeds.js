343db9aac56a17da0a62737dc3b41ba9ce7f5086
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-06 11:42:16 -0600

    editComment-seeds

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,17 +1,17 @@
 const { Comment } = require('../models');
 
 const commentData = [{
-        comment_text: "Lorem ipsum dolor sit amet",
+        comment_text: "forever young",
         user_id: 1,
         post_id: 1
     },
     {
-        comment_text: "consectetur adipiscing elit",
+        comment_text: "NFL starts soon",
         user_id: 2,
         post_id: 2
     },
     {
-        comment_text: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        comment_text: "today is a new day",
         user_id: 3,
         post_id: 3
     }
